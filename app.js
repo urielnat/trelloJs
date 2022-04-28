c67f3d1b94ed48c40028407f0b85ff26c985780a
urielnat <nat_7263@hotmail.com>
2022-04-28 15:36:47 -0500

    ejercicio 5

---
+++ app.js
@@ -0,0 +1,20 @@
+require('dotenv').config()
+
+if(!process.env.TOKEN && !process.env.KEY){
+  throw new Error('No hay configuración con Api Key y con Token')
+}
+
+let Trello = require("trello");
+let trello = new Trello(process.env.KEY, process.env.TOKEN);
+
+let cardTitle = `Card Nueva 3 ${new Date()}`
+
+trello.addCard(cardTitle, "LaunchX Card Description", "626aed8cdc091679b5d9a5a1",
+	function (error, trelloCard) {
+		if (error) {
+			console.log('Could not add card:', error);
+		}
+		else {
+			console.log('Added card:', trelloCard);
+		}
+	});
